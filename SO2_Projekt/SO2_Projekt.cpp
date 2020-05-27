f89cfb8147801b16a1c7bd5af0a5223942afa7fe
GhulJr <oskarrek98@gmail.com>
2020-05-27 17:12:12 +0200

    feat: add comaration methods

--- SO2_Projekt/SO2_Projekt.cpp
+++ SO2_Projekt/SO2_Projekt.cpp
@@ -1,7 +1,12 @@
-﻿#include <iostream>
+﻿#pragma once;
+#include <iostream>
 #include <mutex>
 #include <thread>
+#include "Student.cpp"
+#include "Table.cpp"
 using namespace std;
+
+
 
 int main()
 {
